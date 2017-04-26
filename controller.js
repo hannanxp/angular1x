09d4e2bc9e9407ffd1103734b824eb06234e7a30
Hannan Gigih Prastawa <hannanxp@gmail.com>
2017-04-26 11:34:10 +0700

    Component

--- controller.js
+++ controller.js
@@ -1,19 +1,18 @@
-var myApp = angular.module('myApp', []);
+angular.module('myApp', [])
+        .component('twitPage', {
+          templateUrl: 'twit-template.html',
+          controller: function twitController() {
+            this.twits = [
+              "Lorem ipsum dolor sit amet",
+              "Consectetur adipiscing elit",
+              "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+              "Ut enim ad minim veniam",
+              "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+            ];
 
-myApp.controller('TwitController', ['$scope', function ($scope) {
-
-    $scope.twits = [
-      "Lorem ipsum dolor sit amet",
-      "Consectetur adipiscing elit",
-      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-      "Ut enim ad minim veniam",
-      "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
-    ];
-    
-    $scope.shareTwit = function() {
-      $scope.twits.unshift($scope.msg);
-      $scope.msg = "";
-    };
-
-  }]);
-
+            this.shareTwit = function() {
+              this.twits.unshift(this.msg);
+              this.msg = "";
+            };
+          }
+        });
