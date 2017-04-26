812f639dba700b6424ca90ef596567f28f643d86
Hannan Gigih Prastawa <hannanxp@gmail.com>
2017-04-26 13:41:06 +0700

    change twits as object list

--- twit-component.js
+++ twit-component.js
@@ -2,15 +2,48 @@ angular.module('myApp').component('twitPage', {
   templateUrl: 'twit-template.html',
   controller: function twitController() {
     this.twits = [
-      "Lorem ipsum dolor sit amet",
-      "Consectetur adipiscing elit",
-      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-      "Ut enim ad minim veniam",
-      "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+      {
+        "id": 1,
+        "twit": "Lorem ipsum dolor sit amet",
+        "created": "2017-04-16 12:35:00"
+      },
+      {
+        "id": 2,
+        "twit": "Consectetur adipiscing elit",
+        "created": "2017-04-17 11:13:00"
+      },
+      {
+        "id": 3,
+        "twit": "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        "created": "2017-04-18 09:07:00"
+      },
+      {
+        "id": 4,
+        "twit": "Ut enim ad minim veniam",
+        "created": "2017-04-19 15:17:00"
+      },
+      {
+        "id": 5,
+        "twit": "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+        "created": "2017-04-20 21:10:00"
+      }
     ];
 
     this.shareTwit = function () {
-      this.twits.unshift(this.msg);
+      var date = new Date();
+      var ymd = date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
+      
+      var d = new Date(),
+              h = (d.getHours() < 10 ? '0' : '') + d.getHours(),
+              m = (d.getMinutes() < 10 ? '0' : '') + d.getMinutes();
+      var his = h + ':' + m;
+      
+      var obj = {};
+      obj.id = this.twits.length + 1;
+      obj.twit = this.msg;
+      obj.created = ymd + " " + his;
+      
+      this.twits.unshift(obj);
       this.msg = "";
     };
   }
